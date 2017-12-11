4eec2a95e53b3ca3b098fef8b81a5ca4dedeefb5
Oji Ofong <ojiofong@gmail.com>
2017-12-11 11:36:14 -0500

    Added a solution to convert number to words

--- integer/Solution.java
+++ integer/Solution.java
@@ -6,6 +6,7 @@ public class Solution {
 		System.out.println("reverseNumber-> " + reverseNumber(-123));
 		System.out.println("isPalindromeNumber-> " + isPalindromeNumber(1));
 		System.out.println("convertStringToNumber-> " + convertStringToNumber("123"));
+		System.out.println("convertNumberToWords-> " + convertNumberToWords(1100000));
 	}
 
 	/*-
@@ -48,43 +49,94 @@ public class Solution {
 		// E.g. Int16 - 63556 will (6553 == 6355) rev's 1st digit comes from
 		// last digit of copy
 	}
-	
+
 	/*-
 	 * Convert String to integer Number.
 	 * O(n) time and O(1) space.
 	 */
 	private static int convertStringToNumber(String str) {
-        if (str == null) return 0;
-        str = str.trim();
-        if(str.isEmpty()) return 0;
-        int startIndex = 0;
-        long sum = 0;
-        int sign = 1;
-        char[] arr = str.toCharArray();
-        char firstChar = arr[0];
-        
-        if (firstChar == '-'){
-            sign = -1;
-            startIndex = 1;
-        } else if (firstChar == '+'){
-            sign = 1;
-            startIndex = 1;
-        } 
-        
-        for(int i=startIndex; i < arr.length; i++){
-            char c = arr[i];
-            if (!Character.isDigit(c)){
-                return (int) sum * sign;
-            }
-            sum = (sum * 10) + (c - '0');
-            if (sign == 1 && sum > Integer.MAX_VALUE){
-                return Integer.MAX_VALUE;
-            }
-            if (sign == -1 && (-1 * sum) < Integer.MIN_VALUE){
-                return Integer.MIN_VALUE;
-            }
-        }
-        return (int) sum * sign;
-    }
+		if (str == null)
+			return 0;
+		str = str.trim();
+		if (str.isEmpty())
+			return 0;
+		int startIndex = 0;
+		long sum = 0;
+		int sign = 1;
+		char[] arr = str.toCharArray();
+		char firstChar = arr[0];
+
+		if (firstChar == '-') {
+			sign = -1;
+			startIndex = 1;
+		} else if (firstChar == '+') {
+			sign = 1;
+			startIndex = 1;
+		}
+
+		for (int i = startIndex; i < arr.length; i++) {
+			char c = arr[i];
+			if (!Character.isDigit(c)) {
+				return (int) sum * sign;
+			}
+			sum = (sum * 10) + (c - '0');
+			if (sign == 1 && sum > Integer.MAX_VALUE) {
+				return Integer.MAX_VALUE;
+			}
+			if (sign == -1 && (-1 * sum) < Integer.MIN_VALUE) {
+				return Integer.MIN_VALUE;
+			}
+		}
+		return (int) sum * sign;
+	}
+
+	/*-
+	 * Convert Number to words.
+	 * Input: 123456789
+	 * Output: One hundred twenty three million four hundred fifty six thousand seven hundred eighty nine
+	 * O(n/1000) time and O(1) space.
+	 */
+	private static String convertNumberToWords(int num) {
+		String[] specialNames = { "", " thousand", " million", " billion", " trillion", " quadrillion",
+				" quintillion" };
+		int places = 0;
+		String current = "";
+		while (num > 0) {
+			current = convertLessThan1000(num % 1000) + specialNames[places] + current;
+			num = num / 1000;
+			places = places + 1;
+		}
+
+		return current;
+	}
+
+	private static String convertLessThan1000(int num) {
+		if (num > 999)
+			throw new IllegalArgumentException("num cannot be more than 999");
+
+		String[] tensNames = { "", " ten", " twenty", " thirty", " forty", " fifty", " sixty", " seventy", " eighty",
+				" ninety" };
+		String[] unitNames = { "", " one", " two", " three", " four", " five", " six", " seven", " eight", " nine",
+				" ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen", " seventeen",
+				" eighteen", " nineteen" };
+		String current;
+
+		if (num % 100 <= 19) {
+			current = unitNames[num % 100];
+			num = num / 100;
+		} else {
+			current = unitNames[num % 10]; // get unit digit before tenth
+			num = num / 10;
+			current = tensNames[num % 10] + current; // get tens digit
+			num = num / 10;
+		}
+
+		if (num != 0) {
+			// Number did not stop at tenth. It is in the hundreds
+			current = unitNames[num % 10] + " hundred" + current;
+		}
+
+		return current;
+	}
 
 }

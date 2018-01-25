f64139bba1b704940e180453a67360b6144bbfe5
Oji Ofong <ojiofong@gmail.com>
2018-01-25 08:19:27 -0500

    Updated solution to removeNthLastNode

--- zzz/Test2.java
+++ zzz/Test2.java
@@ -1,116 +1,75 @@
 package zzz;
 
-import tree.BTreePrinter;
-import tree.BinarySTree;
-import tree.Node;
+import java.util.Arrays;
 
 public class Test2 {
 
 	public static void main(String[] args) throws Exception {
-
 		System.out.println(Test2.class.getSimpleName());
-		AVL bst = new AVL();
-		bst.root = bst.insert(1, bst.root);
-		bst.root = bst.insert(2, bst.root);
-		bst.root = bst.insert(3, bst.root);
-		
-		
-		
-		System.out.println("size-> " + bst.size);
-		System.out.println("root null-> " + (bst.root == null));
-		BinarySTree.levelOrder(bst.root);
-		BTreePrinter.printNode(bst.root);
+		int[] arr = { 3, 2, 5, 0, 1, 4 };
+		System.out.println(Arrays.toString(arr));
+//		mergeSort(arr);
+		quickSort(arr);
+		System.out.println(Arrays.toString(arr));
 
 	}
 
-	static class AVL {
-		// private static class Node {
-		// int data;
-		// int ht;
-		// Node left, right;
-		//
-		// public Node(int data) {
-		// this.data = data;
-		// }
-		// }
+	public static void mergeSort(int[] arr) {
+		merge(arr, 0, arr.length - 1);
+	}
 
-		Node root;
-		int size;
+	public static void quickSort(int[] arr) {
+		quick(arr, 0, arr.length - 1);
+	}
 
-		public AVL() {
-			root = null;
-			size = 0;
+	private static void merge(int[] arr, int lo, int hi) {
+		if (lo < hi) {
+			int mid = lo + (hi - lo) / 2;
+			merge(arr, lo, mid);
+			merge(arr, mid + 1, hi);
+			mergeAll(arr, lo, mid, hi);
+		}
+	}
+
+	private static void mergeAll(int[] arr, int lo, int mid, int hi) {
+		int[] helper = Arrays.copyOf(arr, arr.length);
+
+		int i = lo;
+		int j = mid + 1;
+		int k = lo;
+
+		while (i <= mid && j <= hi) {
+			arr[k++] = helper[i] < helper[j] ? helper[i++] : helper[j++];
 		}
 
-		public Node insert(int data) {
-			return insert(data, root);
+		while (i <= mid) {
+			arr[k++] = helper[i++];
 		}
+	}
 
-		private Node insert(int data, Node root) {
-			Node newNode = new Node(data);
-
-			if (root == null) {
-				root = newNode;
-			} else if (data < root.data) {
-				root.left = insert(data, root.left);
-				if (isUnbalanced(root)) {
-					root = data < root.left.data ? rotateWithLeft(root) : doubleWithLeft(root);
-				}
-			} else {
-				root.right = insert(data, root.right);
-				if (isUnbalanced(root)) {
-					root = data < root.right.data ? rotateWithRight(root) : doubleWithRight(root);
-				}
-			}
-
-			size += 1;
-			root.ht = height(root);
-			return root;
+	private static void quick(int[] arr, int lo, int hi) {
+		int i = lo;
+		int j = hi;
+		int pivot = arr[lo + (hi - lo) / 2];
+		
+		while (i <= j) {
+			while (arr[i] < pivot)
+				i++;
+			while (arr[j] > pivot)
+				j--;
+			if (i <= j)
+				swap(arr, i++, j--);
 		}
+		if (i <= hi)
+			quick(arr, i, hi);
+		if (j >= lo)
+			quick(arr, lo, j);
+	}
 
-		private int height(Node n) {
-			if (n == null)
-				return -1;
-			if (n.left == null && n.right == null)
-				return 0;
-			return 1 + Math.max(height(n.left), height(n.right));
-		}
-
-		private boolean isUnbalanced(Node n) {
-			return Math.abs(height(n.left) - height(n.right)) > 1;
-		}
-
-		private boolean isUnbalanced2(Node n) {
-			return Math.abs(n.left.ht - n.right.ht) > 1;
-		}
-
-		private Node rotateWithLeft(Node root) {
-			Node n = root.left;
-			root.left = n.right;
-			n.right = root;
-			root.ht = height(root);
-			n.ht = height(n);
-			return n;
-		}
-
-		private Node rotateWithRight(Node root) {
-			Node n = root.right;
-			root.right = n.left;
-			n.left = root;
-			root.ht = height(root);
-			n.ht = height(n);
-			return n;
-		}
-
-		private Node doubleWithLeft(Node root) {
-			root.left = rotateWithRight(root.left);
-			return rotateWithLeft(root);
-		}
-
-		private Node doubleWithRight(Node root) {
-			root.right = rotateWithLeft(root.right);
-			return rotateWithRight(root);
-		}
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 
 }

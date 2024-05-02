d8647b457e2186d5d48dc19c34d3006c50d3f0f0
Oji Ofong <ojiofong@gmail.com>
2024-05-02 14:59:26 -0500

    Update Solution.java with Merge Sorted Array

--- arrays/Solution.java
+++ arrays/Solution.java
@@ -492,4 +492,50 @@ public class Solution {
 	        return longest;
     }
 
+   /**
+   	Merge Sorted Array 
+	You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+	
+	Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+	
+	The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+	
+	Example 1:
+	Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+	Output: [1,2,2,3,5,6]
+	
+	Example 2:
+	Input: nums1 = [1], m = 1, nums2 = [], n = 0
+	Output: [1]
+	
+	Example 3:
+	Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+	Output: [1]
+   */
+   public void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        // Pointer for the end of the merged array
+        int i = m - 1; // End of nums1 array that is filled
+        int j = n - 1; // End of nums2
+        int k = m + n - 1; // End of merged array
+        
+        // Merge in reverse order
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        // Fill nums1 with remaining nums2 elements
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+
 }// End of class

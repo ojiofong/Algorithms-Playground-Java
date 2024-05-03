ef951f3d7790cdd3799c5d0ae2d47237649f98f2
Oji Ofong <ojiofong@gmail.com>
2024-05-02 21:19:37 -0500

    Update Solution.java: Add Two Numbers in reverse given 2 LinkedLists

--- linkedlists/Solution.java
+++ linkedlists/Solution.java
@@ -384,4 +384,68 @@ public class Solution {
 		return start.next; // return head
 	}
 
+   /**
+     Add Two Numbers in reverse given 2 LinkedLists 
+     
+    You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Example 1:
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+    
+    Example 2:
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+    
+    Example 3:
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+    
+    https://leetcode.com/problems/add-two-numbers/
+ */
+    public ListNode addTwoNumbersInReverse(ListNode l1, ListNode l2) {
+      ListNode cur1 = l1;
+      ListNode cur2 = l2;
+      ListNode resultHead = null;
+      ListNode resultCur = null;
+      int carry = 0;
+    
+      // We're adding digits in reverse order i.e. carrying over from leftmost to rightmost 
+      while (cur1 != null || cur2 != null){
+          int v1 = cur1 != null ? cur1.val : 0;
+          int v2 = cur2 != null ? cur2.val : 0;
+          int sum = v1 + v2 + carry;
+          
+          if (sum > 9){
+              carry = 1;
+              sum = sum % 10;
+          } else {
+              carry = 0;
+          }
+          
+          ListNode newNode = new ListNode(sum);
+          
+          if (resultHead == null){
+              resultHead = newNode;
+              resultCur = newNode;
+          }else {
+              resultCur.next = newNode;
+              resultCur = newNode;
+          }
+          
+          // continue to next avoiding null pointer
+          cur1 = cur1 != null ? cur1.next : cur1;
+          cur2 = cur2 != null ? cur2.next : cur2;
+      }
+        
+      if (carry > 0) {
+          resultCur.next = new ListNode(carry);
+      }
+        
+      return resultHead; 
+    }
+
 }// End of class
